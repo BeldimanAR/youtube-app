986999c97cfdd600f0cf4159e965f6ce64e01e11
Alexandru R. Beldiman <roberto.beldiman10@gmail.com>
2022-12-17 11:16:01 +0200

    navbar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" } }}></Box>
+    </Stack>
+  );
 };
 
 export default Feed;

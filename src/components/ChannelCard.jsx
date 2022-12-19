0776cbeef70059f46a0882e82e4454d9184bcf47
Alexandru R. Beldiman <roberto.beldiman10@gmail.com>
2022-12-19 17:24:12 +0200

    channel card

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = () => {
-  return <div>ChannelCard</div>;
+const ChannelCard = ({ channelDetail }) => {
+  return (
+    <Box sx={{ boxShadow: "none", borderRadius: "20px" }}>
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}></Link>
+    </Box>
+  );
 };
 
 export default ChannelCard;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,13 +3,15 @@ import ChannelCard from "./ChannelCard";
 import VideoCard from "./VideoCard";
 
 const Videos = ({ videos }) => {
-  //   console.log(videos);
+  console.log(videos);
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
       {videos.map((item, index) => (
         <Box key={index}>
           {item.id.videoId && <VideoCard video={item} />}
-          {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+          {/* {item.id.playlistId && <VideoCard video={item} >} */}
+
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
     </Stack>
